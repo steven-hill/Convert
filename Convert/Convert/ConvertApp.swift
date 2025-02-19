7129afc82855e176624329a44cfeb48f8f384f01
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2025-02-19 21:13:50 +0000

    initial commit
    New Xcode project created.

---
+++ Convert/Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Steven Hill on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
